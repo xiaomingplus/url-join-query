f3413e8ff43af85a9c8bd544002c26ab449c74ea
小明plus <xiaomingplus@qq.com>
2016-09-04 19:02:24 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var url = require('url')
+var qs = require('querystring');
+module.exports = function test(){
+  if(arguments.length===0){
+    throw('miss params');
+    return;
+  }else if(arguments.length===1){
+    return arguments[0];
+  }else{
+    var paramsString = "";
+    if(arguments[1]){
+    if(typeof arguments[1]==="string"){
+      paramsString = qs.stringify(qs.parse(arguments[1]));
+    }else if(typeof arguments[1]==='object'){
+      paramsString = qs.stringify(arguments[1]);
+    }else{
+      throw('prams type error');
+      return;
+    }
+    var baseUrl = url.parse(arguments[0],true);
+    var urlQueryString = qs.stringify(baseUrl.query);
+    var queryString = (paramsString || urlQueryString)?((urlQueryString && paramsString)?("?"+urlQueryString+"&"+paramsString):(urlQueryString?("?"+urlQueryString):(paramsString?("?"+paramsString):""))):"";
+    return baseUrl.protocol+"//"+baseUrl.host+(baseUrl.port?(":"+baseUrl.port):"")+baseUrl.pathname+queryString+(baseUrl.hash?baseUrl.hash:"");
+  }else{
+    return arguments[0]
+  }
+}
+};
---
+++ test.js
@@ -0,0 +1,17 @@
+var urlJoinQuery = require('./index');
+var url1 = "http://google.com?a=1&b=2&c=3#hash";
+var url2 = "http://google.com"
+var r1 = urlJoinQuery(url1,"d=4&e=5");
+console.log(r1);//http://google.com/?a=1&b=2&c=3&d=4&e=5#hash
+
+
+var r2 =urlJoinQuery(url1,{d:4,e:5,f:[6,7,8]});
+console.log(r2);//http://google.com/?a=1&b=2&c=3&d=4&e=5&f=6&f=7&f=8#hash
+
+
+var r3 = urlJoinQuery(url2,"d=4&e=5");
+console.log(r3);//http://google.com/?d=4&e=5
+
+
+var r4 =urlJoinQuery(url2,{d:4,e:5,f:[6,7,8]});
+console.log(r4);//http://google.com/?d=4&e=5&f=6&f=7&f=8
